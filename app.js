bca99d9fde8bb32375cbff69d9fcb71948aad068
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-11-26 20:22:48 -0500

    prepping for launch

---
+++ app.js
@@ -0,0 +1,20 @@
+const { exec } = require("child_process");
+const logger = require("./logger");
+
+cron.schedule('* */12 * * *', async () => {
+  
+  exec("./getStats.sh", (err, stdout, stderr) => {
+    if (err) {
+      logger.error("SysAdmin Error: ", err);
+      return;
+    }
+    if (stderr){
+        logger.error("SysAdmin Error (STD): ", stderr);
+        return;
+    }
+  
+    logger.info(`System update on ${moment().format(llll)} :`, stdout);
+  
+  });
+  
+};
---
+++ getStats.sh
@@ -0,0 +1,49 @@
+nodeRows=$(ps -aux | grep node | awk {'print $3,$4'});
+nodeMem=0;
+nodeCpu=0;
+count=0;
+
+for row in $nodeRows;
+do
+  if [ $count -eq 0 ]; then
+    nodeMem=$(echo "$nodeMem + $row" | bc);
+    count=$(expr $count + 1)
+  else
+    nodeCpu=$(echo "$nodeCpu + $row" | bc);
+    count=0;
+  fi
+done;
+
+rows=$(ps -aux | grep node -v | awk 'NR>1 {print $3,$4'});
+mem=0;
+cpu=0;
+count=0;
+for row in $rows;
+do
+  if [ $count -eq 0 ]; then
+    mem=$(echo "$mem + $row" | bc);
+    count=$(expr $count + 1)
+  else
+    cpu=$(echo "$cpu + $row" | bc);
+    count=0;
+  fi
+done;
+
+totalRows=$(ps -aux);
+numProcesses=$(echo "$totalRows" | wc -l);
+numNodeProcesses=$(echo "$totalRows" | grep node | wc -l);
+numNonNodeProcesses=$(echo "$numProcesses - $numNodeProcesses" | bc);
+totalCpu=$(echo "$nodeCpu + $cpu" | bc);
+totalMem=$(echo "$nodeMem + $mem" | bc);
+
+echo "Total processes: $numProcesses";
+echo "Total memory: $totalMem";
+echo "Total CPU: $totalCpu";
+echo
+echo "Node processes: $numNodeProcesses";
+echo "Node CPU: $nodeCpu";
+echo "Node memory: $nodeMem";
+echo
+echo "Other processes: $numNonNodeProcesses";
+echo "Other CPU: $cpu";
+echo "Other memory: $mem";
