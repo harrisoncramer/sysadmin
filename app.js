8f2c08a5b555e81263732cc670d2e6f9b8af3411
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-11-26 20:27:20 -0500

    fixed typo

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const { exec } = require("child_process");
 const logger = require("./logger");
 const cron = require("node-cron");
 
+logger.info("System booting up...")
+
 cron.schedule('* */12 * * *', async () => {
 
+  logger.info("Running checks...");
+  
   exec("./getStats.sh", (err, stdout, stderr) => {
     if (err) {
       logger.error("SysAdmin Error: ", err);
